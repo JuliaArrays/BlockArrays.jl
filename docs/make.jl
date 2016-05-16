6bac00a4dc9d382010615d0447592ff558a96855
Kristoffer Carlsson <kristoffer.carlsson@chalmers.se>
2016-05-16 17:49:15 +0200

    modernize doc gen

--- docs/make.jl
+++ docs/make.jl
@@ -13,11 +13,8 @@ makedocs(
 # Deploy built documentation from Travis.
 # =======================================
 
-# Needs to install an additional dep, mkdocs-material, so provide a custom `deps`.
-custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
-
 deploydocs(
     # options
-    deps = custom_deps,
+    deps = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/KristofferC/BlockArrays.jl.git"
 )
