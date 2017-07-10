f0ce6972b79fce2a1b7718b1735dd0b84e3deae6
maximerischard <rischardmaxime@gmail.com>
2017-07-09 23:17:26 -0700

    remove trailing whitespaces

--- src/blocktridiag.jl
+++ src/blocktridiag.jl
@@ -1,6 +1,3 @@
-# Note: Functions surrounded by a comment blocks are there because `Vararg` is still allocating.
-# When Vararg is fast enough, they can simply be removed.
-
 #=======================
  BlockTridiagMatrix
 =======================#
@@ -8,11 +5,11 @@
 """
     BlockTridiagMatrix{T, R <: AbstractMatrix{T}} <: AbstractBlockArray{T, N}
 
-A `BlockTridiagMatrix` is a block tridiagonal matrix where each block is stored contiguously. 
+A `BlockTridiagMatrix` is a block tridiagonal matrix where each block is stored contiguously.
 This means that insertions and retrieval of blocks
 can be very fast and non allocating since no copying of data is needed.
 
-In the type definition, `R` defines the array type that each block has, 
+In the type definition, `R` defines the array type that each block has,
 for example `Matrix{Float64}.
 """
 struct BlockTridiagMatrix{T, R <: AbstractMatrix{T}} <: AbstractBlockMatrix{T}
@@ -24,16 +21,19 @@ end
 
 # Auxilary outer constructors
 function BlockTridiagMatrix{T, R <: AbstractArray{T}
-                           }(diagl::Vector{R}, 
-                             lower::Vector{R}, 
+                           }(diagl::Vector{R},
+                             lower::Vector{R},
                              upper::Vector{R},
                              block_sizes::BlockSizes{2})
    return BlockTridiagMatrix{T, R}(diagl, lower, upper, block_sizes)
 end
 
-function BlockTridiagMatrix{T, R <: AbstractArray{T}}(diagl::Vector{R}, lower::Vector{R}, upper::Vector{R}, 
-                            block_sizes::Vararg{Vector{Int}, 2})
-    return BlockTridiagMatrix{T, R}(diagl, lower, upper, 
+function BlockTridiagMatrix{T, R <: AbstractArray{T}
+                           }(diagl::Vector{R},
+                             lower::Vector{R},
+                             upper::Vector{R},
+                             block_sizes::Vararg{Vector{Int}, 2})
+    return BlockTridiagMatrix{T, R}(diagl, lower, upper,
                                     BlockSizes(block_sizes...))
 end
 
@@ -43,7 +43,7 @@ end
 ################
 
 """
-Constructs a `BlockTridiagMatrix` with uninitialized blocks from a block type `R` 
+Constructs a `BlockTridiagMatrix` with uninitialized blocks from a block type `R`
 with sizes defind by `block_sizes`.
 
 ```jldoctest
@@ -58,7 +58,7 @@ julia> BlockTridiagMatrix(Matrix{Float64}, [1,3], [2,2])
 ```
 """
 @inline function BlockTridiagMatrix{T, R <: AbstractMatrix{T}
-                                   }(::Type{R}, 
+                                   }(::Type{R},
                                      block_sizes::Vararg{Vector{Int}, 2})
     BlockTridiagMatrix(R, BlockSizes(block_sizes...))
 end
@@ -72,7 +72,7 @@ function BlockTridiagMatrix{T, R <: AbstractMatrix{T}}(::Type{R}, block_sizes::B
     BlockTridiagMatrix{T,R}(diagl, lower, upper, block_sizes)
 end
 
-function BlockTridiagMatrix(arr::AbstractMatrix, 
+function BlockTridiagMatrix(arr::AbstractMatrix,
                             block_sizes::Vararg{Vector{Int}, 2})
     for i in 1:2
         if sum(block_sizes[i]) != size(arr, i)
@@ -145,7 +145,7 @@ end
 # AbstractArray Interface #
 ###########################
 
-@inline function Base.similar{T2}(bltrid_mat::BlockTridiagMatrix, 
+@inline function Base.similar{T2}(bltrid_mat::BlockTridiagMatrix,
                                     ::Type{T2})
     diagl = bltrid_mat.diagl
     lower = bltrid_mat.lower
@@ -157,7 +157,7 @@ end
 end
 
 function Base.size(arr::BlockTridiagMatrix)
-    return (arr.block_sizes[1][end]-1, 
+    return (arr.block_sizes[1][end]-1,
             arr.block_sizes[2][end]-1)
 end
 
