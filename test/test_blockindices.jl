c22ab56495eae409fea528fae5258659f3de41b5
Robin Deits <robin.deits@gmail.com>
2018-06-21 16:33:57 -0400

    test for zero-allocation

--- test/test_blockindices.jl
+++ test/test_blockindices.jl
@@ -23,14 +23,24 @@ block_size = BlockSizes([1,2,3], [2, 3])
 @test @inferred(globalrange(block_size, (2,1))) == (2:3, 1:2)
 @test @inferred(globalrange(block_size, (2,2))) == (2:3, 3:5)
 
+# Test for allocations inside a function to avoid noise due to global 
+# variable references
+wrapped_allocations = (bs, i) -> @allocated(globalrange(bs, i))
+@test wrapped_allocations(block_size, (1, 1)) == 0
+
 @test @inferred(global2blockindex(block_size, (3, 1))) == BlockIndex((2,1), (2,1))
 @test @inferred(global2blockindex(block_size, (1, 4))) == BlockIndex((1,2), (1,2))
 @test @inferred(global2blockindex(block_size, (4, 5))) == BlockIndex((3,2), (1,3))
+
+wrapped_allocations = (bs, i) -> @allocated(global2blockindex(bs, i))
+@test wrapped_allocations(block_size, (3, 1)) == 0
 
 @test @inferred(blockindex2global(block_size, BlockIndex((2,1), (2,1)))) == (3, 1)
 @test @inferred(blockindex2global(block_size, BlockIndex((1,2), (1,2)))) == (1, 4)
 @test @inferred(blockindex2global(block_size, BlockIndex((3,2), (1,3)))) == (4, 5)
 
+wrapped_allocations = (bs, i) -> @allocated(blockindex2global(bs, i))
+@test wrapped_allocations(block_size, BlockIndex((2,1), (2,1))) == 0
 
 @test block_size == BlockSizes(1:3, 2:3)
 
