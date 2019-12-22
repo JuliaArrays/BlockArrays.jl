e8b12d35abbe16abd5b4790cf1c77606005ebefc
Sheehan Olver <solver@mac.com>
2019-12-22 06:34:19 -0800

    Update test_blockarrayinterface.jl

--- test/test_blockarrayinterface.jl
+++ test/test_blockarrayinterface.jl
@@ -5,26 +5,14 @@ struct PartiallyImplementedBlockVector <: AbstractBlockArray{Float64,1} end
 @testset "partially implemented block array" begin
     # the error thrown before was incorrect
     A = PartiallyImplementedBlockVector()
-    try
-        getblock(A, 1)
-    catch err
-        @test err isa ErrorException && err.msg == "getblock for PartiallyImplementedBlockVector is not implemented"
-    end
-    try
-        getblock!(zeros(5), A, Block(1))
-    catch err
-        @test err isa ErrorException && err.msg == "getblock! for PartiallyImplementedBlockVector is not implemented"
-    end
-    try
-        BlockArrays.setblock!(A, zeros(5), Block(1))
-    catch err
-        @test err isa ErrorException && err.msg == "setblock! for PartiallyImplementedBlockVector is not implemented"
-    end
-    try
-        BlockArrays.axes(A)
-    catch err
-        @test err isa ErrorException && err.msg == "axes for PartiallyImplementedBlockVector is not implemented"
-    end
+    @test_throws(ErrorException("getblock for PartiallyImplementedBlockVector is not implemented"),
+                getblock(A, 1))
+    @test_throws(ErrorException("getblock! for PartiallyImplementedBlockVector is not implemented"),
+        getblock!(zeros(5), A, Block(1)))
+    @test_throws(ErrorException("setblock! for PartiallyImplementedBlockVector is not implemented"),
+        BlockArrays.setblock!(A, zeros(5), Block(1)))
+    @test_throws(ErrorException( "axes for PartiallyImplementedBlockVector is not implemented"),
+        BlockArrays.axes(A))
 end
 
 @testset "Array block interface" begin
