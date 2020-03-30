c470f2d0237b43527a1799821e19701fb97a7e11
David Hong <dahong67@wharton.upenn.edu>
2020-03-30 13:20:40 -0400

    Move eachblock tests to test_blockindices.jl

--- test/test_blockindices.jl
+++ test/test_blockindices.jl
@@ -272,3 +272,18 @@ end
     @test !BlockArrays.sortedin(0,v)
     @test !BlockArrays.sortedin(5,v)
 end
+
+@testset "eachblock" begin
+    v = Array(reshape(1:6, (2, 3)))
+    A = BlockArray(v, [1,1], [2,1])
+    B = PseudoBlockArray(v, [1,1], [2,1])
+
+    # test that contents match
+    @test collect(eachblock(A)) == collect(eachblock(B)) == A.blocks
+
+    # test that eachblock returns views
+    first(eachblock(A))[1,2] = 0
+    @test A[1,2] == 0
+    first(eachblock(B))[1,2] = 0
+    @test B[1,2] == 0
+end
