1036d20d7794c9b95e300ce341a81f4b6b2181b3
Takafumi Arakaki <aka.tkf@gmail.com>
2020-05-16 17:40:42 -0700

    Run tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using BlockArrays, LinearAlgebra, Test
     include("test_blockindices.jl")
     include("test_blockarrays.jl")
     include("test_blockviews.jl")
+    include("test_blocks.jl")
     include("test_blockrange.jl")
     include("test_blockarrayinterface.jl")
     include("test_blockbroadcast.jl")
