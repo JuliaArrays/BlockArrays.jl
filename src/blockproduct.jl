af3717c4ca5dd55ed25bb41b9deb7b82814d4565
Sheehan Olver <solver@mac.com>
2020-06-15 10:29:22 +0100

    Start work on blockkron

--- src/blockproduct.jl
+++ src/blockproduct.jl
@@ -28,3 +28,68 @@ end
 function khatri_rao(A::AbstractMatrix, B::AbstractMatrix)
     kron(A, B)
 end
+
+""""
+    BlockKron(A...)
+
+creates a lazy representation of kron(A...) with the natural
+block-structure imposed. This is a component in `blockkron(A...)`.
+"""
+struct BlockKron{T,N,ARGS<:Tuple} <: LayoutArray{T,N}
+    args::ARGS
+end
+
+BlockKron{T}(A...) where {T} = BlockKron{T,typeof(A)}(A)
+BlockKron(A...) = BlockKron{mapreduce(eltype,promote_type,A)}(A...)
+
+
+size(B::BlockKron) = size(Kron(B))
+
+kron_getindex((A,)::Tuple{AbstractVector}, k::Integer) = A[k]
+function kron_getindex((A,B)::NTuple{2,AbstractVector}, k::Integer)
+    K,κ = divrem(k-1, length(B))
+    A[K+1]*B[κ+1]
+end
+kron_getindex((A,)::Tuple{AbstractMatrix}, k::Integer, j::Integer) = A[k,j]
+function kron_getindex((A,B)::NTuple{2,AbstractVecOrMat}, k::Integer, j::Integer)
+    K,κ = divrem(k-1, size(B,1))
+    J,ξ = divrem(j-1, size(B,2))
+    A[K+1,J+1]*B[κ+1,ξ+1]
+end
+
+kron_getindex(args::Tuple, k::Integer, j::Integer) = kron_getindex(tuple(Kron(args[1:2]...), args[3:end]...), k, j)
+kron_getindex(args::Tuple, k::Integer) = kron_getindex(tuple(Kron(args[1:2]...), args[3:end]...), k)
+
+getindex(K::BlockKron{<:Any,1}, k::Integer) = kron_getindex(K.args, k)
+getindex(K::BlockKron{<:Any,2}, k::Integer, j::Integer) = kron_getindex(K.args, k, j)
+
+function axes(K::BlockKron)
+    A,B = K.args
+    blockedrange.((Fill(size(B,1), size(A,1)), Fill(size(B,2), size(A,2))))
+end
+
+const SubKron{T,M1,M2,R1,R2} = SubArray{T,2,<:BlockKron{T,M1,M2},<:Tuple{<:BlockSlice{R1},<:BlockSlice{R2}}}
+
+
+BroadcastStyle(::Type{<:SubKron{<:Any,<:Any,B,Block1,Block1}}) where B =
+    BroadcastStyle(B)
+
+
+# allow dispatch on memory layout
+_blockkron(_, A) = BlockArray(BlockKron(A...))
+
+
+""""
+    blockkron(A...)
+
+creates a blocked version of kron(A...) with the natural
+block-structure imposed. 
+"""
+blockkron(A...) = _blockkron(map(MemoryLayout,A), A)
+
+"""
+    blockvec(A::AbstractMatrix)
+
+creates a blocked version of `vec(A)`, with the block structure used to represent the columns.
+"""
+blockvec(A::AbstractMatrix) = PseudoBlockVector(vec(A), Fill(size(A,1), size(A,2)))
