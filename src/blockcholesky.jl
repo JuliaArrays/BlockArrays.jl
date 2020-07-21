5605d8f14d25f8d1945fe52da5fbf7686b095a58
Juntian Wang <296705853yt@gmail.com>
2020-07-21 22:31:09 +0100

    Added lower Cholesky factorization

--- src/blockcholesky.jl
+++ src/blockcholesky.jl
@@ -1,45 +1,83 @@
 
+
 ##########################
 # Cholesky Factorization #
 ##########################
 
 """
 Funtions to do
-1.cholesky structure
 2.check square
 3.check positive definite
-4.swap 'cholesky!'
 """
 
 cholesky(A::Symmetric{<:Real,<:BlockArray},
     ::Val{false}=Val(false); check::Bool = true) = cholesky!(cholcopy(A); check = check)
 
-function cholesky!(A::Symmetric{<:Real,<:BlockArray}; check::Bool = true)
-    
-    chol_P = parent(A)
 
-    # Initializing the first role of blocks
-    cholesky!(Symmetric(getblock(chol_P,1,1)))
-    for j = 2:blocksize(A)[1]
-        ldiv!(UpperTriangular(getblock(chol_P,1,1))', getblock(chol_P,1,j))
-    end
+function b_chol!(A::BlockArray{T}, ::Type{UpperTriangular}) where T<:Real
+    n = blocksize(A)[1]
 
-    # For the left blocks
-    for i = 2:blocksize(A)[1]
-        Pii = getblock(chol_P,i,i) 
-        for k = 1:i-1
-            mul!(Pii,getblock(chol_P,k,i)',getblock(chol_P,k,i),-1.0,1.0)
+    @inbounds begin
+        # Initializing the first role of blocks
+        cholesky!(Symmetric(getblock(A,1,1)))
+        for j = 2:n
+            ldiv!(UpperTriangular(getblock(A,1,1))', getblock(A,1,j))
         end
-        cholesky!(Symmetric(Pii))
 
-        for j = i+1:blocksize(A)[1]
-            Pij = getblock(chol_P,i,j)
+        # For the left blocks
+        for i = 2:n
+            Pii = getblock(A,i,i) 
             for k = 1:i-1
-                mul!(Pij,getblock(chol_P,k,i)',getblock(chol_P,k,j),-1.0,1.0)
+                muladd!(-1.0, getblock(A,k,i)', getblock(A,k,i), 1.0, Pii)
             end
-            ldiv!(UpperTriangular(getblock(chol_P,i,i))', Pij)
+            cholesky!(Symmetric(Pii))
+    
+            for j = i+1:n
+                Pij = getblock(A,i,j)
+                for k = 1:i-1
+                    muladd!(-1.0, getblock(A,k,i)', getblock(A,k,j), 1.0, Pij)
+                end
+                ldiv!(UpperTriangular(getblock(A,i,i))', Pij)
+            end
         end
     end
-    
-    return UpperTriangular(chol_P)
+
+    return UpperTriangular(A)
 end
+function b_chol!(A::BlockArray{T}, ::Type{LowerTriangular}) where T<:Real
+    n = blocksize(A)[1]
+
+    @inbounds begin
+        # Initializing the first role of blocks
+        cholesky!(Symmetric(getblock(A,1,1), :L))
+        for j = 2:n
+            rdiv!(getblock(A,j,1), LowerTriangular(getblock(A,1,1))')
+        end
+
+        # For the left blocks
+        for i = 2:n
+            Pii = getblock(A,i,i) 
+            for k = 1:i-1
+                muladd!(-1.0, getblock(A,i,k), getblock(A,i,k)', 1.0, Pii)
+            end
+            cholesky!(Symmetric(Pii, :L))
+    
+            for j = i+1:n
+                Pij = getblock(A,j,i)
+                for k = 1:i-1
+                    muladd!(-1.0, getblock(A,j,k), getblock(A,i,k)', 1.0, Pij)
+                end
+                rdiv!(Pij, LowerTriangular(getblock(A,i,i))')
+            end
+        end
+    end
+
+    return LowerTriangular(A)
+end
+
+function cholesky!(A::Symmetric{<:Real,<:BlockArray}, ::Val{false}=Val(false); check::Bool = true)
+    C = b_chol!(A.data, A.uplo == 'U' ? UpperTriangular : LowerTriangular)
+    #check && checkpositivedefinite(info)
+    return Cholesky(C.data, A.uplo, 0)
+end
+
--- test/test_cholesky.jl
+++ test/test_cholesky.jl
@@ -7,17 +7,29 @@ using BlockArrays, Test, LinearAlgebra
     # Generating random positive definite and symmetric matrices
     A = BlockArray{Float64}(randn(9,9)+100I, fill(3,3), fill(3,3)); A = Symmetric(A)
     B = BlockArray{Float64}(randn(55,55)+100I, 1:10, 1:10); B = Symmetric(B)
+    C = BlockArray{Float64}(randn(9,9)+100I, fill(3,3), fill(3,3)); C = Symmetric(C, :L)
+    D = BlockArray{Float64}(randn(55,55)+100I, 1:10, 1:10); D = Symmetric(D, :L)
 
     A_T = Matrix(A)
     B_T = Matrix(B)
+    C_T = Matrix(C)
+    D_T = Matrix(D)
 
     #Tests on A
-    @test UpperTriangular(cholesky(A)) ≈ cholesky(A_T).U
-    @test UpperTriangular(cholesky(A))'UpperTriangular(cholesky(A)) ≈ A
+    @test cholesky(A).U ≈ cholesky(A_T).U
+    @test cholesky(A).U'cholesky(A).U ≈ A
     
     #Tests on B
-    @test UpperTriangular(cholesky(B)) ≈ cholesky(B_T).U
-    @test UpperTriangular(cholesky(B))'UpperTriangular(cholesky(B)) ≈ B
+    @test cholesky(B).U ≈ cholesky(B_T).U
+    @test cholesky(B).U'cholesky(B).U ≈ B
+
+    #Tests on C
+    @test cholesky(C).L ≈ cholesky(C_T).L
+    @test cholesky(C).L*cholesky(C).L' ≈ C
+
+    #tests on D
+    @test cholesky(D).L ≈ cholesky(D_T).L
+    @test cholesky(D).L*cholesky(D).L' ≈ D
 
 end
 
