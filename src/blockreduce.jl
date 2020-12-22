b7a6301d723e80d6625880488a3dfd629be79b50
Sheehan Olver <solver@mac.com>
2020-12-22 10:57:28 +0000

    Fix #141

--- src/blockreduce.jl
+++ src/blockreduce.jl
@@ -16,3 +16,6 @@ Base.mapfoldl(f::F, op::OP, B::PseudoBlockArray; kw...) where {F, OP} =
 
 Base.mapreduce(f::F, op::OP, B::PseudoBlockArray; kw...) where {F, OP} =
     mapreduce(f, op, B.blocks; kw...)
+
+# support sum, need to return something analoguous to Base.OneTo(1) but same type
+Base.reduced_index(::BR) where BR<:BlockedUnitRange = convert(BR, Base.OneTo(1))
