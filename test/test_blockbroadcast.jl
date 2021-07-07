ed1ebb15b6ac5ceef5b7d5f784733290d8e49c96
Sheehan Olver <solver@mac.com>
2021-07-07 10:19:17 +0100

    remove LazyArray-dependent test

--- test/test_blockbroadcast.jl
+++ test/test_blockbroadcast.jl
@@ -1,4 +1,4 @@
-using BlockArrays, FillArrays, LazyArrays, Test
+using BlockArrays, FillArrays, Test
 import BlockArrays: SubBlockIterator, BlockIndexRange, Diagonal
 
 @testset "broadcast" begin
@@ -169,32 +169,6 @@ import BlockArrays: SubBlockIterator, BlockIndexRange, Diagonal
         @test blockisequal(axes(A .* Ones(6)), axes(A .* ones(6)))
         @test blockisequal(axes(A .* Zeros(axes(A))), axes(Zeros(axes(A)) .* A), axes(A .* zeros(6)))
         @test blockisequal(axes(A .* Ones(axes(A))), axes(Ones(axes(A)) .* A), axes(A .* ones(6)))
-    end
-
-    @testset "recurrences" begin
-        N = 1000
-        n = mortar(BroadcastArray(Fill,Base.OneTo(N),Base.OneTo(N)))
-        k = mortar(BroadcastArray(Base.OneTo,Base.OneTo(N)))
-
-        @test view(n, Block(5)) ≡ Fill(5,5)
-        @test view(k,Block(5)) ≡ Base.OneTo(5)
-        a = b = c = 0.0
-        # for some reason the following was causing major slowdown. I think it 
-        # went pass a limit to Base.Broadcast.flatten which caused `bc.f` to have a strange type.
-        # bc = Base.Broadcast.instantiate(Base.broadcasted(/, Base.broadcasted(*, k, Base.broadcasted(-, Base.broadcasted(-, k, n), a)), Base.broadcasted(+, 2k, b+c-1)))
-
-        bc = Base.Broadcast.instantiate(Base.broadcasted((k,n,a,b,c) -> k * (k-n-a) / (2k+(b+c-1)), k, n, a, b, c))
-        @test axes(n,1) ≡ axes(k,1) ≡ axes(bc)[1] ≡ blockedrange(Base.OneTo(N))
-        u = (k .* (k .- n .- a) ./ (2k .+ (b+c-1)))
-        @test u == (Vector(k) .* (Vector(k) .- Vector(n) .- a) ./ (2Vector(k) .+ (b+c-1)))
-        @test copyto!(u, bc) == (k .* (k .- n .- a) ./ (2k .+ (b+c-1)))
-        @test @allocated(copyto!(u, bc)) ≤ 1000 
-        # not clear why allocatinos so high: all allocations are coming from checking
-        # axes
-
-        u = PseudoBlockArray{Float64}(undef, collect(1:N))
-        @test copyto!(u, bc) == (k .* (k .- n .- a) ./ (2k .+ (b+c-1)))
-        @test @allocated(copyto!(u, bc)) ≤ 1000
     end
 
     @testset "type inferrence" begin
