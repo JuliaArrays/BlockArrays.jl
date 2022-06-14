2d43bda017e100fd015eb9dd08c3c53f66cb4785
Sheehan Olver <solver@mac.com>
2022-06-14 16:50:47 +0100

    Update blockcholesky.jl

--- src/blockcholesky.jl
+++ src/blockcholesky.jl
@@ -4,7 +4,7 @@
 ##########################################
 
 
-function _diag_chol!(A::AbstractArray{T}, i::Int, ::Type{UpperTriangular}) where T<:Real
+function _diag_chol!(A::AbstractArray{T}, i::Int, uplo) where T<:Real
     Pii = view(A,Block(i,i))
     for k = Int(first(blockcolsupport(A,Block(i)))):i-1
         muladd!(-one(T), view(A,Block(k,i))', view(A,Block(k,i)), one(T), Pii)
@@ -12,13 +12,6 @@ function _diag_chol!(A::AbstractArray{T}, i::Int, ::Type{UpperTriangular}) where
     return LAPACK.potrf!('U', Pii)
 end
 
-function _diag_chol!(A::AbstractArray{T}, i::Int, ::Type{LowerTriangular}) where T<:Real
-    Pii = view(A,Block(i,i))
-    for k = 1:i-1
-        muladd!(-one(T), view(A,Block(k,i))', view(A,Block(k,i)), one(T), Pii)
-    end
-    return LAPACK.potrf!('U', Pii)
-end
 
 function _nondiag_chol!(A::AbstractArray{T}, i::Int, n::Int, ::Type{UpperTriangular}) where T<:Real
     for j = i+1:Int(last(blockrowsupport(A,Block(i))))
@@ -31,9 +24,9 @@ function _nondiag_chol!(A::AbstractArray{T}, i::Int, n::Int, ::Type{UpperTriangu
 end
 
 function _nondiag_chol!(A::AbstractArray{T}, i::Int, n::Int, ::Type{LowerTriangular}) where T<:Real
-    for j = intersect(convert(Array{Int,1},blockrowsupport(A,i)),i+1:n)
+    for j = i+1:Int(last(blockrowsupport(A,Block(i))))
         Pij = view(A,Block(i,j))
-        for k = 1:i-1
+        for k = Int(first(blockcolsupport(A,Block(j)))):i-1
             muladd!(-one(T), view(A,Block(k,i))', view(A,Block(k,j)), one(T), Pij)
         end
         ldiv!(UpperTriangular(view(A,Block(i,i)))', Pij)
