15996336593cab068745c948fad1b337cdad8acf
Sheehan Olver <solver@mac.com>
2023-03-31 21:11:30 +0100

    Avoid ambiguity in view triggered in BlockBandedMatrices.jl (#253)

--- src/blocklinalg.jl
+++ src/blocklinalg.jl
@@ -103,12 +103,8 @@ sub_materialize(::ArrayLayouts.OnesLayout, V, ax::Tuple{<:AbstractUnitRange,<:Bl
 sub_materialize(::ArrayLayouts.ZerosLayout, V, ax::Tuple{<:AbstractUnitRange,<:BlockedUnitRange}) =
     Zeros{eltype(V)}(ax)
 
-@propagate_inbounds Base.view(A::FillArrays.AbstractFill{<:Any,N}, I::Vararg{Union{Real, AbstractArray, Block}, N}) where N =
+@propagate_inbounds Base.view(A::FillArrays.AbstractFill, I::Union{Real, AbstractArray, Block}...) =
     FillArrays._fill_getindex(A, Base.to_indices(A, I)...)
-@propagate_inbounds Base.view(A::FillArrays.AbstractFill{<:Any,N}, I::Block{N}) where N =
-    FillArrays._fill_getindex(A, Base.to_indices(A, (I,))...)
-@propagate_inbounds Base.view(A::FillArrays.AbstractFill{<:Any,1}, I::Block{1}) =
-    FillArrays._fill_getindex(A, Base.to_indices(A, (I,))...)
 
 conjlayout(::Type{T}, ::BlockLayout{MLAY,BLAY}) where {T<:Complex,MLAY,BLAY} = BlockLayout{MLAY,typeof(conjlayout(T,BLAY()))}()
 conjlayout(::Type{T}, ::BlockLayout{MLAY,BLAY}) where {T<:Real,MLAY,BLAY} = BlockLayout{MLAY,BLAY}()
