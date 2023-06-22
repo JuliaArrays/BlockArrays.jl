b6f0d1273f07d0162a69994f75175d2493bfc086
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-06-22 18:01:26 +0530

    remove missed doctest setup

--- src/blockindices.jl
+++ src/blockindices.jl
@@ -5,7 +5,7 @@ A `Block` is simply a wrapper around a set of indices or enums so that it can be
 indexing a `AbstractBlockArray` with a `Block` the a block at that block index will be returned instead of
 a single element.
 
-```jldoctest; setup = quote using BlockArrays end
+```jldoctest
 julia> A = BlockArray(ones(2,3), [1, 1], [2, 1])
 2×2-blocked 2×3 BlockMatrix{Float64}:
  1.0  1.0  │  1.0
@@ -120,7 +120,7 @@ and the offset index into the block.
 
 It can be used to index into `BlockArrays` in the following manner:
 
-```jldoctest; setup = quote using BlockArrays end
+```jldoctest
 julia> arr = Array(reshape(1:25, (5,5)));
 
 julia> a = PseudoBlockArray(arr, [3,2], [1,4])
