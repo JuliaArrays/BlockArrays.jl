549a1e1aa0e551b8a03e5e4b4be1fe2a0699530e
Charles Kawczynski <kawczynski.charles@gmail.com>
2023-10-11 10:09:50 -0700

    Fix upstream errors

--- src/blockbroadcast.jl
+++ src/blockbroadcast.jl
@@ -30,10 +30,12 @@ BroadcastStyle(::PseudoBlockStyle{M}, ::BlockStyle{N}) where {M,N} = BlockStyle(
 
 # sortedunion can assume inputs are already sorted so this could be improved
 sortedunion(a,b) = sort!(union(a,b))
-sortedunion(a::Tuple, b::Tuple) = (a..., b...)
+sortedunion_tuple(a::Tuple, b::Tuple) = (a..., b...)
 sortedunion(a::Base.OneTo, b::Base.OneTo) = Base.OneTo(max(last(a),last(b)))
 sortedunion(a::AbstractUnitRange, b::AbstractUnitRange) = min(first(a),first(b)):max(last(a),last(b))
-combine_blockaxes(a, b) = _BlockedUnitRange(sortedunion(blocklasts(a), blocklasts(b)))
+combine_blockaxes(a, b) = combine_blockaxes(a, b, blocklasts(a), blocklasts(b))
+combine_blockaxes(a, b, abl, bbl) = length(b) == 1 ? a : _BlockedUnitRange(sortedunion(abl, bbl))
+combine_blockaxes(a, b, abl::Tuple, bbl::Tuple) = _BlockedUnitRange(sortedunion_tuple(abl, bbl))
 
 Base.Broadcast.axistype(a::BlockedUnitRange, b::BlockedUnitRange) = combine_blockaxes(a, b)
 Base.Broadcast.axistype(a::BlockedUnitRange, b) = combine_blockaxes(a, b)
