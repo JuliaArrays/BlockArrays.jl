ea33af1a5c321f4f7cbaa6afad567acd67403973
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-03-21 00:47:46 +0530

    Fix examples in manual (#341)

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,9 @@ end
 using Documenter
 @testset "docstrings" begin
     # don't test docstrings on old versions to avoid failures due to changes in types
-    if VERSION >= v"1.9"
+    if v"1.10" <= VERSION < v"1.11.0-"
         DocMeta.setdocmeta!(BlockArrays, :DocTestSetup, :(using BlockArrays); recursive=true)
-        doctest(BlockArrays)
+        doctest(BlockArrays, manual=false)
     end
 end
 
