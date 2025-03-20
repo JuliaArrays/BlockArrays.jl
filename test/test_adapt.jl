3ceec8aafe65243a42999314740182562ddc6692
mtfishman <mfishman@flatironinstitute.org>
2025-03-20 16:02:55 -0400

    Include Adapt tests

--- test/test_adapt.jl
+++ test/test_adapt.jl
@@ -3,12 +3,12 @@ module TestBlockArraysAdapt
 using BlockArrays, Adapt, Test
 
 @testset "Adapt" begin
-    @testset "Ranges" begin
+    @testset "Adapt Ranges" begin
         @test blockisequal(adapt(Array, blockedrange([2, 3])), blockedrange([2, 3]))
         @test blockisequal(adapt(Array, blockedrange(2, [2, 3])), blockedrange(2, [2, 3]))
     end
 
-    @testset "Block Arrays" begin
+    @testset "Adapt Block Arrays" begin
         a = BlockArray(randn(4, 4), [2, 2], [2, 2])
         @test blockisequal(adapt(Array, a), a)
         @test blockisequal(adapt(Array, view(a, :, :)), a)
