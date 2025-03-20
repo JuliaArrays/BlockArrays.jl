1b27cc2cbca9c123a71d59e00d6a10fc7ba3a058
mtfishman <mfishman@flatironinstitute.org>
2025-03-20 16:19:02 -0400

    Add adapt definitions for block arrays

--- ext/BlockArraysAdaptExt.jl
+++ ext/BlockArraysAdaptExt.jl
@@ -2,10 +2,13 @@ module BlockArraysAdaptExt
 
 using Adapt
 using BlockArrays
-using BlockArrays: _BlockedUnitRange
+using BlockArrays: _BlockArray, _BlockedUnitRange
 import Adapt: adapt_structure
 
 adapt_structure(to, r::BlockedUnitRange) = _BlockedUnitRange(adapt(to, r.first), map(adapt(to), r.lasts))
 adapt_structure(to, r::BlockedOneTo) = BlockedOneTo(map(adapt(to), r.lasts))
+
+adapt_structure(to, A::BlockArray) = _BlockArray(map(adapt(to), blocks(A)), map(adapt(to), axes(A)))
+adapt_structure(to, A::BlockedArray) = BlockedArray(adapt(to, A.blocks), map(adapt(to), axes(A)))
 
 end
