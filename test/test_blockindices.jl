761c119e1ce6aa455dca93a08193041db11d783c
mtfishman <mfishman@flatironinstitute.org>
2025-07-10 12:48:26 -0400

    Improve test coverage

--- test/test_blockindices.jl
+++ test/test_blockindices.jl
@@ -886,19 +886,23 @@ end
 end
 
 @testset "NoncontiguousBlockSlice" begin
-    b = NoncontiguousBlockSlice([Block(2),Block(1)], mortar([3:5,1:2]))
-    @test length(b) == 5
-    for i in eachindex(b.indices)
-        @test b[i] === b.indices[i]
+    bs = NoncontiguousBlockSlice([Block(2),Block(1)], mortar([3:5,1:2]))
+    @test length(bs) ≡ 5
+    for i in eachindex(bs.indices)
+        @test bs[i] ≡ bs.indices[i]
     end
-    @test b[Block(1)] === BlockSlice(Block(2), 3:5)
-    @test b[Block(2)] === BlockSlice(Block(1), 1:2)
-    @test BlockArrays._indices(b) == mortar([3:5,1:2])
+    @test bs[Block(1)] ≡ BlockSlice(Block(2), 3:5)
+    @test bs[Block(2)] ≡ BlockSlice(Block(1), 1:2)
+    @test BlockArrays._indices(bs) == mortar([3:5,1:2])
 
     b = NoncontiguousBlockSlice(Block(3), 2:4)
-    @test b[2:3] == NoncontiguousBlockSlice(Block(3)[3:4], 3:4)
-    @test Block(b) === Block(3)
-    @test BlockArrays._indices(b) === 2:4
+    @test b[2:3] ≡ NoncontiguousBlockSlice(Block(3)[2:3], 3:4)
+    @test Block(b) ≡ Block(3)
+    @test BlockArrays._indices(b) ≡ 2:4
+
+    bir = NoncontiguousBlockSlice(Block(3)[3:5], 4:6)
+    @test Block(bir) ≡ Block(3)
+    @test bir[2:3] ≡ NoncontiguousBlockSlice(Block(3)[4:5], 5:6)
 end
 
 #=
