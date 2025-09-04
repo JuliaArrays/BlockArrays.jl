JuliaArrays/BlockArrays.jl

commit 4d1e02f9763512a896581604ac9743f23269c3c3

lower to standard broadcast for PseudoBlockArray (#193)
* lower to standard broadcast for PseudoBlockArray

* Update blockbroadcast.jl

* Update blockbroadcast.jl

* Update test_blockbroadcast.jl

* Update test_blockbroadcast.jl

* v0.16.8

src/blockbroadcast.jl
@@ -220,11 +220,19 @@ function copyto!(dest::AbstractVector,
     end
     return _fast_blockbradcast_copyto!(dest, bc)
 end
-@inline function Broadcast.instantiate(bc::Broadcasted{Style}) where {Style <:AbstractBlockStyle}
+@inline function Broadcast.instantiate(bc::Broadcasted{Style}) where {Style <:BlockStyle}
     bcf = Broadcast.instantiate(Broadcast.flatten(Broadcasted{Nothing}(bc.f, bc.args, bc.axes)))
     return Broadcasted{Style}(bcf.f, bcf.args, bcf.axes)
 end
 
+_removeblocks(a::Broadcasted) = broadcasted(a.f, map(_removeblocks,a.args)...)
+_removeblocks(a::PseudoBlockArray) = a.blocks
+_removeblocks(a::BlockSlice) = a.indices
+_removeblocks(a::Adjoint) = _removeblocks(parent(a))'
+_removeblocks(a::Transpose) = transpose(_removeblocks(parent(a)))
+_removeblocks(a::SubArray{<:Any,N,<:PseudoBlockArray}) where N = view(_removeblocks(parent(a)), map(_removeblocks, parentindices(a))...)
+_removeblocks(a) = a
+copy(bc::Broadcasted{PseudoBlockStyle{N}}) where N = PseudoBlockArray(materialize(_removeblocks(bc)), axes(bc))
 
 for op in (:+, :-, :*)
     @eval function copy(bc::Broadcasted{BlockStyle{N},<:Any,typeof($op),<:Tuple{<:AbstractArray{<:Number,N}}}) where N

test/test_blockbroadcast.jl
@@ -50,6 +50,13 @@ import BlockArrays: SubBlockIterator, BlockIndexRange, Diagonal
         @test axes(B .+ 1) == axes(B)
         @test axes(A .+ 1 .+ B) == axes(B)
         @test A .+ 1 .+ B == Vector(A) .+ 1 .+ B == Vector(A) .+ 1 .+ Matrix(B)
+
+        @testset "preserve structure" begin
+             x = PseudoBlockArray(1:6, Fill(3,2))
+             @test x + x isa PseudoBlockVector{Int,<:AbstractRange}
+             @test 2x + x isa PseudoBlockVector{Int,<:AbstractRange}
+             @test 2 .* (x .+ 1) isa PseudoBlockVector{Int,<:AbstractRange}
+        end
     end
 
     @testset "Mixed" begin
@@ -184,9 +191,11 @@ import BlockArrays: SubBlockIterator, BlockIndexRange, Diagonal
         @test Base.BroadcastStyle(typeof(a')) isa BlockArrays.PseudoBlockStyle{2}
         @test Base.BroadcastStyle(typeof(b')) isa BlockArrays.BlockStyle{2}
 
-        @test exp.(a') == exp.(b') == exp.(Vector(a)')
+        @test exp.(a') == exp.(b') == exp.(transpose(a)) == exp.(transpose(b)) == exp.(Vector(a)')
         @test exp.(a') isa PseudoBlockArray
+        @test exp.(transpose(a)) isa PseudoBlockArray
         @test exp.(b') isa BlockArray
+        @test exp.(transpose(b)) isa BlockArray
     end
 
     @testset "subarray" begin

Project.toml
@@ -1,6 +1,6 @@
 name = "BlockArrays"
 uuid = "8e7c35d0-a365-5155-bbbb-fb81a777f24e"
-version = "0.16.7"
+version = "0.16.8"
 
 [deps]
 ArrayLayouts = "4c555306-a7a7-4459-81d9-ec55ddd5c99a"

commit 9c5639d392b02cd11de1624493665b5dff1bb59f

Fix nested broadcasting of BlockedArray (#485)
Fixes #295. That issue seemed to be caused by code in the generic block
broadcasting logic such as
https://github.com/JuliaArrays/BlockArrays.jl/blob/90ddfa96fb0ade3da4f67ac285fcb340350c19c6/src/blockbroadcast.jl#L121-L129
assuming the broadcasting expression is already flat. This PR adds a
call to `Broadcast.flatten` to explicitly flatten the broadcast
expression in the generic block broadcast code.

It appears that at some point `BlockedStyle` broadcasting expressions
were being flattened in this `Broadcast.instantiate` definition:
https://github.com/JuliaArrays/BlockArrays.jl/blob/90ddfa96fb0ade3da4f67ac285fcb340350c19c6/src/blockbroadcast.jl#L199-L202
but that was changed in #193. Reverting that change broke the tests
introduced in #193 so I guess it is not safe to do that.

I have to admit I'm not sure if this is the best place to call
`Broadcast.flatten`, a lot of this broadcasting code is hard to follow.
The place I put it in this PR fixes #295, doesn't break other tests, and
is hopefully least likely to interfere with custom implementations of
block broadcasting in downstream packages.

src/blockbroadcast.jl
@@ -138,9 +138,10 @@ end
 end
 
 function _generic_blockbroadcast_copyto!(dest::AbstractArray,
-                            bc::Broadcasted{<:AbstractBlockStyle{NDims}, <:Any, <:Any, Args}) where {NDims, Args <: Tuple}
+                            bc1::Broadcasted{<:AbstractBlockStyle{NDims}}) where {NDims}
 
-    NArgs = fieldcount(Args)
+    bc = Broadcast.flatten(bc1)
+    NArgs = length(bc.args)
 
     bs = axes(bc)
     if !blockisequal(axes(dest), bs)

test/test_blockbroadcast.jl
@@ -67,10 +67,26 @@ using StaticArrays
         @test A .+ 1 .+ B == Vector(A) .+ 1 .+ B == Vector(A) .+ 1 .+ Matrix(B)
 
         @testset "preserve structure" begin
-             x = BlockedArray(1:6, Fill(3,2))
-             @test x + x isa BlockedVector{Int,<:AbstractRange}
-             @test 2x + x isa BlockedVector{Int,<:AbstractRange}
-             @test 2 .* (x .+ 1) isa BlockedVector{Int,<:AbstractRange}
+            x = BlockedArray(1:6, Fill(3,2))
+            @test x + x isa BlockedVector{Int,<:AbstractRange}
+            @test 2x + x isa BlockedVector{Int,<:AbstractRange}
+            @test 2 .* (x .+ 1) isa BlockedVector{Int,<:AbstractRange}
+        end
+
+        @testset "nested in-place broadcast" begin
+            x = BlockedVector(randn(4), [2, 2])
+            y = BlockedVector(randn(4), [2, 2])
+            dest = copy(x)
+            dest .+= 2 .* y
+            @test dest ≈ x + 2y
+        end
+
+        @testset "0-dim nested in-place broadcast" begin
+            x = BlockedArray(randn(()))
+            y = BlockedArray(randn(()))
+            dest = copy(x)
+            dest .+= 2 .* y
+            @test dest ≈ x + 2y
         end
     end
 