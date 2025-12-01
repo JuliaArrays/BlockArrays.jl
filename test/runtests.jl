cd73466e5e1783cf6176a0091a5838c1f2a49798
Jishnu Bhattacharya <jishnub.github@gmail.com>
2025-12-01 22:58:27 +0530

    Run tests in parallel (#496)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,32 +1,15 @@
-using BlockArrays, LinearAlgebra, Test
+using BlockArrays
+using ParallelTestRunner
 
-using Aqua
-downstream_test = "--downstream_integration_test" in ARGS
-@testset "Project quality" begin
-    Aqua.test_all(BlockArrays, ambiguities=false,
-        stale_deps=!downstream_test)
+# Start with autodiscovered tests
+testsuite = find_tests(pwd())
+
+if "--downstream_integration_test" in ARGS
+    delete!(testsuite, "test_aqua")
 end
 
-using Documenter
-@testset "docstrings" begin
-    # don't test docstrings on old versions to avoid failures due to changes in types
-    if v"1.10" <= VERSION < v"1.11.0-"
-        DocMeta.setdocmeta!(BlockArrays, :DocTestSetup, :(using BlockArrays); recursive=true)
-        doctest(BlockArrays, manual=false)
-    end
-end
+filtered_args = filter(!=("--downstream_integration_test"), ARGS)
+# Parse arguments
+args = parse_args(filtered_args)
 
-include("test_blockindices.jl")
-include("test_blockarrays.jl")
-include("test_blockviews.jl")
-include("test_blocks.jl")
-include("test_blockrange.jl")
-include("test_blockarrayinterface.jl")
-include("test_blockbroadcast.jl")
-include("test_blocklinalg.jl")
-include("test_blockproduct.jl")
-include("test_blockreduce.jl")
-include("test_blockdeque.jl")
-include("test_blockcholesky.jl")
-include("test_blockbanded.jl")
-include("test_adapt.jl")
+runtests(BlockArrays, args; testsuite)
